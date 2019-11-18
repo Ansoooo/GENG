b3ea1b0625c51996bb2f18ea0d8d8459dc8f52df
Egor Lukoyanov <egor.lukoyanov@ontariotech.com>
2019-11-18 16:26:04 -0500

    Object Pooling
    Added object pooling for rocket launcher for the player. Still need to fix how it shoots rockets, so its attached to the player.

---
+++ Assignment1-master/A1/Assets/Scripts/RocketLauncher/TestManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PoolManager.instance.CreatePool(prefab, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            PoolManager.instance.ReuseObject(prefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
---
+++ Assignment1-master/A1/Assets/Scripts/RocketLauncher/TestObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : PoolObject
+{
+    TrailRenderer trail;
+    float trailTime;
+
+    private void Awake()
+    {
+        trail = GetComponent<TrailRenderer>();
+        trailTime = trail.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 25);
+    }
+
+    public override void OnObjectReuse()
+    {
+        trail.time = -1;
+        Invoke("ResetTrail", .1f);
+    }
+
+    void ResetTrail()
+    {
+        trail.time = trailTime;
+    }
+
+}
+
