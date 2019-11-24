3d348b8677d15b53763bf6e8a139d0e6a0b1f331
ZunVGC <Y_1st.Zenith@iCloud.com>
2019-11-24 09:05:30 -0500

    SaveLoadPosition 2.0

---
+++ Assignment1-master/A1/Assets/Scripts/SaveLoadPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            SaveAndLoad.Save("SaveData/save.gsave", transform.position.x.ToString() + "," + transform.position.y.ToString() + "," + transform.position.z.ToString());
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            string[] content = new string[0];
+            SaveAndLoad.Load("SaveData/save.gsave", ref content);
+            transform.position = new Vector3(float.Parse(content[0]), float.Parse(content[1]), float.Parse(content[2]));
+        }
+    }
+}
