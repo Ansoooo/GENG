132645b9017776a2cfeb3ea65cd9d07f4278184f
ZunVGC <Y_1st.Zenith@iCloud.com>
2019-11-23 19:39:25 -0500

    Save Load Player Position 1.0

---
+++ Assignment1-master/A1/Assets/Scripts/SaveAndLoad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class SaveAndLoad
+{
+    public static void Save(string path, string content)
+    {
+        File.WriteAllText(Application.dataPath + "/" + path, content);
+    }
+
+    public static void Load(string path, ref string[] content)
+    {
+        using(StreamReader sr = new StreamReader(Application.dataPath + "/" + path))
+        {
+            content = sr.ReadToEnd().Split(',');
+            sr.Close();
+        }
+    }
+}
