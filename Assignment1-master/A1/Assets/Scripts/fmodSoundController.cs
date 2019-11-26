1eacddb43716372587e10d705ad0cbf57c9b59ba
Egor Lukoyanov <egor.lukoyanov@ontariotech.com>
2019-11-25 22:26:10 -0500

    added FMOD

---
+++ Assignment1-master/A1/Assets/Scripts/fmodSoundController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fmodSoundController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/punch");
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/RocketLaunch");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Jump");
+        }
+    }
+}
