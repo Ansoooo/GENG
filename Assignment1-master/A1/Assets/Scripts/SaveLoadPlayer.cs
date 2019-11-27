1f182402c172e2b3e675fc6fe866cabe82109de9
ZunVGC <Y_1st.Zenith@iCloud.com>
2019-11-27 14:43:56 -0500

    Save and Load Done

--- Assignment1-master/A1/Assets/Scripts/SaveLoadPlayer.cs
+++ Assignment1-master/A1/Assets/Scripts/SaveLoadPlayer.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class SaveLoadPlayer : MonoBehaviour
 {
+    public float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 5.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timer <= 0)
+        {
+            timer = 0;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+
         if(Input.GetKeyDown(KeyCode.C))
         {
             SaveAndLoad.Save("SaveData/save.gsave", transform.position.x.ToString() + "," + transform.position.y.ToString() + "," + transform.position.z.ToString());
         }
 
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X) && timer <= 0)
         {
+            timer = 5.0f;
             string[] content = new string[0];
             SaveAndLoad.Load("SaveData/save.gsave", ref content);
             transform.position = new Vector3(float.Parse(content[0]), float.Parse(content[1]), float.Parse(content[2]));
