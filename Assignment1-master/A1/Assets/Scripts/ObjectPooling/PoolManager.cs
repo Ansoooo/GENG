2ff4b2ded0c8d2315886902962d1d8e8ec230eae
Alvin <alvin.aniwa@ontariotechu.net>
2019-12-02 13:14:15 -0500

    Added FMOD and editted object pooling

--- Assignment1-master/A1/Assets/Scripts/ObjectPooling/PoolManager.cs
+++ Assignment1-master/A1/Assets/Scripts/ObjectPooling/PoolManager.cs
@@ -2,6 +2,50 @@
 using System.Collections;
 using System.Collections.Generic;
 
+    public class ObjectInstance
+    {
+
+        GameObject gameObject;
+        Transform transform;
+
+        bool hasPoolObjectComponent;
+        PoolObject poolObjectScript;
+
+        public ObjectInstance(GameObject objectInstance)
+        //Creates an instance of the game object to be pooled
+        {
+            gameObject = objectInstance;
+            transform = gameObject.transform;
+            gameObject.SetActive(false);
+
+            if (gameObject.GetComponent<PoolObject>())
+            {
+                hasPoolObjectComponent = true;
+                poolObjectScript = gameObject.GetComponent<PoolObject>();
+            }
+        }
+
+        public void Reuse(Vector3 position, Quaternion rotation)
+        //Allows objects belonging to the pool to be reused
+        {
+            gameObject.SetActive(true);
+            transform.position = position;
+            transform.rotation = rotation;
+
+            if (hasPoolObjectComponent)
+            {
+                poolObjectScript.OnObjectReuse();
+            }
+        }
+
+        public void SetParent(Transform parent)
+        {
+            transform.parent = parent;
+        }
+    }
+
+//The pool manager controls the functions of the pool.
+//It specifies how many objects need to be pooled and what to do with them upon emptying of the pool.
 public class PoolManager : MonoBehaviour
 {
 
@@ -22,75 +66,37 @@ public class PoolManager : MonoBehaviour
     }
 
     public void CreatePool(GameObject prefab, int poolSize)
+        //Creates an instance of the pool
     {
-        int poolKey = prefab.GetInstanceID();
+        int poolRef = prefab.GetInstanceID();
 
-        if (!poolDictionary.ContainsKey(poolKey))
+        if (!poolDictionary.ContainsKey(poolRef))
         {
-            poolDictionary.Add(poolKey, new Queue<ObjectInstance>());
+            poolDictionary.Add(poolRef, new Queue<ObjectInstance>());
 
-            GameObject poolHolder = new GameObject(prefab.name + " pool");
-            poolHolder.transform.parent = transform;
+            GameObject poolContainer = new GameObject(prefab.name + " pool");
+            poolContainer.transform.parent = transform;
 
             for (int i = 0; i < poolSize; i++)
             {
                 ObjectInstance newObject = new ObjectInstance(Instantiate(prefab) as GameObject);
-                poolDictionary[poolKey].Enqueue(newObject);
-                newObject.SetParent(poolHolder.transform);
+                poolDictionary[poolRef].Enqueue(newObject);
+                newObject.SetParent(poolContainer.transform);
             }
         }
     }
 
     public void ReuseObject(GameObject prefab, Vector3 position, Quaternion rotation)
     {
-        int poolKey = prefab.GetInstanceID();
+        int poolRef = prefab.GetInstanceID();
 
-        if (poolDictionary.ContainsKey(poolKey))
+        if (poolDictionary.ContainsKey(poolRef))
         {
-            ObjectInstance objectToReuse = poolDictionary[poolKey].Dequeue();
-            poolDictionary[poolKey].Enqueue(objectToReuse);
+            ObjectInstance objectToReuse = poolDictionary[poolRef].Dequeue();
+            poolDictionary[poolRef].Enqueue(objectToReuse);
 
             objectToReuse.Reuse(position, rotation);
         }
     }
 
-    public class ObjectInstance
-    {
-
-        GameObject gameObject;
-        Transform transform;
-
-        bool hasPoolObjectComponent;
-        PoolObject poolObjectScript;
-
-        public ObjectInstance(GameObject objectInstance)
-        {
-            gameObject = objectInstance;
-            transform = gameObject.transform;
-            gameObject.SetActive(false);
-
-            if (gameObject.GetComponent<PoolObject>())
-            {
-                hasPoolObjectComponent = true;
-                poolObjectScript = gameObject.GetComponent<PoolObject>();
-            }
-        }
-
-        public void Reuse(Vector3 position, Quaternion rotation)
-        {
-            gameObject.SetActive(true);
-            transform.position = position;
-            transform.rotation = rotation;
-
-            if (hasPoolObjectComponent)
-            {
-                poolObjectScript.OnObjectReuse();
-            }
-        }
-
-        public void SetParent(Transform parent)
-        {
-            transform.parent = parent;
-        }
-    }
 }
